046d20a0ef988061e3cf20e211bdf6f32ff398e9
Camden <camdenlink7@gmail.com>
2022-08-29 20:37:48 -0400

    working sort!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from asyncio.windows_events import INFINITE
+from pydoc import classname
 from bs4 import BeautifulSoup
 from datetime import datetime
 from dateutil import parser
+
 assignments = []
 
 name_class = "cell-title"
 class_class = "cell-;bwx"
+complete_class = "cell-lR;U"
 due_date_class = "cell-hWhl"
 type_class = "cell-UfRw"
 
@@ -17,17 +20,52 @@ table_rows = table.findChildren("tr")
 
 #HELPERS
 def create_assignment_element(assignment):
-    pass
+    new_row = soup.new_tag("tr")
+
+    #name
+    name = soup.new_tag("td", **{'class': name_class})
+    name.string = assignment['name']
+    new_row.append(name)
+
+    #class
+    course = soup.new_tag("td", **{'class': class_class})
+    course.string = assignment["class"]
+    new_row.append(course)
+
+    #complete
+    is_complete_class = "checkbox-off"
+    if assignment['complete']:
+        is_complete_class = "checkbox-on"
+    complete = soup.new_tag("td", **{'class': complete_class})
+    complete_checkbox = soup.new_tag("div", **{'class': "checkbox " + str(is_complete_class)})
+    complete.append(complete_checkbox)
+    new_row.append(complete)
+
+    #due date
+    due_date = soup.new_tag("td", **{'class': due_date_class})
+    due_date.string = assignment["due_date"].strftime("%b %d, %Y @ %I:%M %p")
+    new_row.append(due_date)
+
+    #type
+    type = soup.new_tag("td", **{'class': type_class})
+    type.string = assignment["type"]
+    new_row.append(type)
+
+
+    #append to the main table
+    table.append(new_row)
+
 
 
 #Note: array must have a length of at least 2
+# @array is an array of assignment objects
 def selection_sort(array):
     for i in range(len(array)):
         low = i
         for x in range(i + 1, len(array)):
-            if array[x] < array[low]:
+            if array[x]['due_date'] < array[low]['due_date']:
                 low = x
-        if array[i] > array[low]:
+        if array[i]['due_date'] > array[low]['due_date']:
             low_value = array[low]
             array[low] = array[i]
             array[i] = low_value
@@ -38,9 +76,14 @@ def selection_sort(array):
 
 #Collect all the assignments and store them as dictionaries in array
 for row in table_rows:
+    is_complete = False
+    if ("checkbox-on" in row.find("td", {"class": complete_class}).find("div")['class']):
+        is_complete = True
+
     assignments.append({
         "name": row.find("td", {"class": name_class}).find("a").text,
         "class": row.find("td",  {"class": class_class}).find("span").text,
+        "complete": is_complete,
         "due_date": row.find("td", {"class": due_date_class}).find("time").text,
         "type": row.find("td", {"class": type_class}).find("span").text
     })
@@ -56,9 +99,13 @@ for assn in assignments:
          due_date = (assn_due_chunked[0] + " " + assn_due_chunked[1] + " " + assn_due_chunked[2])
     assn['due_date'] = parser.parse(due_date)
 
-#Sort the assignment array based on due date comparison (Using Selection Sort
-sorted_assns = []
-for assn in assignments:
-    pass
 
-print(selection_sort([5, 2, 6, 0, 1, -50]))
+table.clear()
+#Sort the assignment array based on due date comparison (Using Selection Sort) and create html elements for each
+for assn in selection_sort(assignments):
+    create_assignment_element(assn)
+
+
+#Save the modified soup to a new html file
+with open("SortedAssignments.html", "w", encoding="utf-8") as file:
+    file.write(str(soup.prettify()))
